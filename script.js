d2f2a68bf5d4f4e22a46c62ae73c3d740cc912fa
arif6371 <143376581+arif6371@users.noreply.github.com>
2024-03-09 19:29:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'f97d519239msh068d03aec790259p15d027jsn09ef9f0ff8cc',
+// 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
